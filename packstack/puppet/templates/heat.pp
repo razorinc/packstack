bbab3e81a78fc37f6b992d6723dc40af62d2a54c
Francesco Vollero <fvollero@redhat.com>
2013-07-02 17:38:07 +0200

    [wip] Adding quotes

--- packstack/puppet/templates/heat.pp
+++ packstack/puppet/templates/heat.pp
@@ -1,11 +1,8 @@
 
 class { 'heat'
-  rabbit_host => %(CONFIG_QPID_HOST)s,  # just showing how to use config values in manifests
+  rabbit_host => "%(CONFIG_QPID_HOST)s",
   
 }
 
-class { 'heat::db::mysql':
-  password => %(CONFIG_MYSQL_HEAT_PW)s,
-}
 
-# You can do whatever you need here
+ You can do whatever you need here
